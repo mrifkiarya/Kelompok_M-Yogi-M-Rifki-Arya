a6b04c9ab88232eaf241f10dde9e45c02cb98d0d
mrifkiarya <ikkiarya546@gmail.com@rifkiarya>
2021-03-24 21:55:44 +0700

    updatemikro

---
+++ Sistem Mikrokontroller/6. Integrasi Sistem Microcontroller Sebagai Display/LCD/2_LCD/2_LCD.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int i1=9;
+int i2=8;
+void setup()
+{
+lcd.begin(16, 2);
+pinMode(i1,INPUT);
+pinMode(i2,INPUT);
+}
+void loop()
+{
+if (digitalRead(i1)==LOW){
+ {
+  lcd.print("tombol 1 ditekan");
+  lcd.setCursor(0, 1);
+ }
+}
+delay(1000);
+if (digitalRead(i2)==LOW){
+  {
+  lcd.print("tombol 2 ditekan"); 
+  lcd.setCursor(0,2);
+  }
+ }
+}
